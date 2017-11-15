54e46c294d475ff8715248a5bb06e2102a3dd7ae
Samuel Lemes <samuellemes25@gmail.com>
2017-11-15 16:24:09 -0200

    Create server.js

--- server.js
+++ server.js
@@ -7,18 +7,18 @@
 //  confifurar o Setup da aplicação.
 
 let express = require('express')
-let app = express();
+let app = express()
 let bodyParser = require('body-parser')
 
 // Configurando a variavel app para usar o 'bodyParser()'. E retornar os dados de um json e via post:
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json)
+app.use(bodyParser.json())
 
 // Definindo a porta onde será executada a api:
 let port = process.env.port || 8000;
 
 // Criando uma instância das rotas via Express:
-let router = express.router();
+let router = express.Router()
 
 // Route test. 
 router.get('/', function(req, res) {
