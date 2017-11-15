dab4da8f91e847a8f8bca7097b3608b4242dc782
Samuel Lemes <samuellemes25@gmail.com>
2017-11-15 17:16:10 -0200

    Changes to the server.js

--- server.js
+++ server.js
@@ -4,25 +4,24 @@
  * Author: Responsável por levantar o serviço do node para que seja possivel executar a api através do Express.
  */
 
-//  confifurar o Setup da aplicação.
-
-let express = require('express')
-let app = express()
-let bodyParser = require('body-parser')
+//  confifurando o Setup da aplicação.
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser')
 
 // Configurando a variavel app para usar o 'bodyParser()'. E retornar os dados de um json e via post:
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 // Definindo a porta onde será executada a api:
-let port = process.env.port || 8000;
+var port = process.env.port || 8000;
 
 // Criando uma instância das rotas via Express:
-let router = express.Router()
+var router = express.Router()
 
 // Route test. 
 router.get('/', function(req, res) {
-    res.json({ message: 'Beleza! Bem vindo(a) a nossa Loja.'})
+    res.json({ message: 'Beleza! Bem vindo(a) a nossa Loja.' })
 });
 
 // definiddo um padrão das rotas prefixadas '/api':
